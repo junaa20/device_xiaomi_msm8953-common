67ad1c014f409e9fc7b5bcd0ee3b49634cd51a94
AlexThundrous <alexthundrous2104@gmail.com>
2020-11-02 06:47:43 +0000

    Change sepolicy path
    Signed-off-by: AlexThundrous <alexthundrous2104@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -187,7 +187,7 @@ TARGET_RECOVERY_FSTAB := $(VENDOR_PATH)/rootdir/fstab.qcom
 endif
 
 #SELinux
-include device/qcom/sepolicy-legacy-um/sepolicy.mk
+include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/vendor
 SELINUX_IGNORE_NEVERALLOWS := true
 
